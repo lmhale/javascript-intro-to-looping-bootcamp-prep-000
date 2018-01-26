a1f323f3ff8c6b440aff6659afa75ef56e62dbc2
Lauren M Hale <lmhale710@gmail.com>
2018-01-26 22:43:09 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,11 +1,13 @@
+
 function forLoop(array){
 
   for(var i = 0; i <= 25;i++){
     if(i === 1){
-      console.log(`I am ${i} strange loop`);
+      console.log("I am 1 strange loop.");
     }else
-    console.log(`I am ${i} strange loops`);
+    console.log(`I am ${i} strange loops.`);
   }
 
   return[];
 }
+forLoop();
